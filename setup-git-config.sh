4daba4c6372a1a6da0bbd4bc6e227d97d821d833
Iulia Bejan <64602043+iulia-b@users.noreply.github.com>
2025-02-07 12:11:04 +0100

    Create setup-git-config.sh

---
+++ setup-git-config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Disable GPG signing for commits globally
+git config --global commit.gpgsign false
+
+# Set the GPG program to /bin/true globally
+git config --global gpg.program /bin/true
